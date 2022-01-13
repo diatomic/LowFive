7114306f94e3d5f8c31036ac64ad6128bf482a27
Dmitriy Morozov <dmitriy@mrzv.org>
2022-01-13 13:49:44 -0800

    Fix enum output after fmtlib upgrade via peru

--- include/lowfive/metadata.hpp
+++ include/lowfive/metadata.hpp
@@ -4,6 +4,19 @@
 #include <string>
 #include <map>
 
+// This is required to format enum classes with fmt as they're not implicitly
+// convertible to int.
+// This operator has to be declared before including format.h and ostream.h.
+// We keep clang format disabled to avoid reordering and breaking things
+// https://github.com/fmtlib/fmt/issues/391
+#include <iostream>
+#include <type_traits>
+template<typename T, typename std::enable_if<std::is_enum<T>::value, int>::type = 0>
+std::ostream& operator<<(std::ostream& os, const T& value)
+{
+  return os << static_cast<int>(value);
+}
+
 #include <fmt/core.h>
 #include <fmt/format.h>
 #include <fmt/ostream.h>
