249e8ee8ca8423f2aaf212638537ca755c47d3aa
Dmitriy Morozov <dmitriy@mrzv.org>
2022-02-14 12:27:19 -0800

    Change b/p/test-basic.py to work with MetadataVOL

--- bindings/python/test-basic.py
+++ bindings/python/test-basic.py
@@ -1,10 +1,15 @@
 import lowfive      # lowfive needs to be imported before h5py
-vol = lowfive.VOLBase()
-# vol = lowfive.MetadataVOL()
+# vol = lowfive.VOLBase()
+vol = lowfive.MetadataVOL()
+vol.set_passthru("*","*")
 
 import h5py
 import numpy as np
 
 f = h5py.File('foo.h5', 'w')
-f['data'] = np.ones((4, 3, 2), 'f')
+f.create_dataset("data", data=np.ones((4, 3, 2), 'f'))
+# f["data"] = np.ones((4, 3, 2), 'f')
 
+# there is something wrong with how destructors are invoked when Python
+# terminates, so for now need to delete f explicitly
+del f
