ccb439a43ad154ca1fa530eb686749c47e2ee732
Tom Peterka <tpeterka@mcs.anl.gov>
2022-02-22 07:32:01 -0600

    Fixed missing pointer in MetadataVOL::link_create
    H5VL_loc_params_t* cur_params

--- src/metadata/link.cpp
+++ src/metadata/link.cpp
@@ -13,17 +13,19 @@ link_create(H5VL_link_create_type_t create_type, void *obj,
 
     herr_t res = 0;
 
+    // see hdf5 src/H5VLnative_link.c, H5VL__native_link_create()
+
     // both metadata and native objects need to be linked, so two separate if statements
     // but first we need to extract the shared arguments
     //
     void* cur_obj;
-    H5VL_loc_params_t cur_params;
+    H5VL_loc_params_t* cur_params;
     char* target_name;
     if(H5VL_LINK_CREATE_HARD == create_type)
     {
         /* Retrieve the object & loc params for the link target */
         cur_obj = va_arg(arguments, void *);
-        cur_params = va_arg(arguments, H5VL_loc_params_t);
+        cur_params = va_arg(arguments, H5VL_loc_params_t*);
     } else if (H5VL_LINK_CREATE_SOFT == create_type)
     {
         target_name = va_arg(arguments, char*);
@@ -40,22 +42,30 @@ link_create(H5VL_link_create_type_t create_type, void *obj,
             ObjectPointers* cur_obj_ = (ObjectPointers*) cur_obj;
             fmt::print(stderr, "  cur_obj = {}\n", *cur_obj_);
 
-            if (cur_params.type == H5VL_OBJECT_BY_SELF)
+            if (cur_params->type == H5VL_OBJECT_BY_SELF)
             {
                 // TODO: probably should handle the path, in case it can be given
                 assert(name.find("/") == std::string::npos);      // expecting just a name, not a path
                 static_cast<Object*>(cur_obj_->mdata_obj)->name = name;
-            } else if (cur_params.type == H5VL_OBJECT_BY_NAME)
+            } else if (cur_params->type == H5VL_OBJECT_BY_NAME)
             {
                 // (obj, loc_params) -> (cur_obj, cur_params)
                 auto obj_path = static_cast<Object*>(obj_->mdata_obj)->locate(*loc_params);
                 assert(obj_path.is_name());
 
-                auto cur_obj_path = static_cast<Object*>(cur_obj_->mdata_obj)->locate(cur_params);
+                auto cur_obj_path = static_cast<Object*>(cur_obj_->mdata_obj)->locate(*cur_params);
                 Object* cur_obj = cur_obj_path.exact();     // assert that we found it
                 obj_path.obj->add_child(new HardLink(obj_path.path, cur_obj));
+            } else if (cur_params->type == H5VL_OBJECT_BY_TOKEN)
+            {
+                // TODO
+                throw MetadataError(fmt::format("link_create(): don't yet support cur_params->type = H5VL_OBJECT_BY_TOKEN"));
+            } else if (cur_params->type == H5VL_OBJECT_BY_IDX)
+            {
+                // TODO
+                throw MetadataError(fmt::format("link_create(): don't yet support cur_params->type = H5VL_OBJECT_BY_IDX"));
             } else
-                throw MetadataError(fmt::format("link_create(): don't recognize cur_params.type = {}", cur_params.type));
+                throw MetadataError(fmt::format("link_create(): don't recognize cur_params->type = {}", cur_params->type));
         } else if(H5VL_LINK_CREATE_SOFT == create_type)
         {
                 // (obj, loc_params) -> target_name
@@ -76,7 +86,7 @@ link_create(H5VL_link_create_type_t create_type, void *obj,
                 cur_obj = unwrap(cur_obj);
 
             res = link_create_trampoline(create_type, unwrap(obj_), loc_params, under_vol_id, lcpl_id, lapl_id, dxpl_id, req,
-                        cur_obj, cur_params);
+                        cur_obj, *cur_params);
         } else if(H5VL_LINK_CREATE_SOFT == create_type)
         {
             res = link_create_trampoline(create_type, unwrap(obj_), loc_params, under_vol_id, lcpl_id, lapl_id, dxpl_id, req, target_name);
