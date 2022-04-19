2a66e20f3aff1c9d5fe7d0c66b36a186629fa0aa
Dmitriy Morozov <dmitriy@mrzv.org>
2022-04-19 11:51:04 -0700

    Add MPIComm and clean up inheritance + initialization

--- bindings/python/lowfive.cpp
+++ bindings/python/lowfive.cpp
@@ -1,4 +1,5 @@
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 namespace py = pybind11;
 
 #include <lowfive/vol-metadata.hpp>
@@ -10,11 +11,13 @@ PYBIND11_MODULE(_lowfive, m)
 
     m.doc() = "LowFive python bindings";
 
-    py::class_<LowFive::VOLBase>(m, "VOLBase", "base VOL object")
+    py::class_<LowFive::VOLBase> vol_base(m, "VOLBase", "base VOL object");
+    vol_base
         .def(py::init<>(), "construct the object")
     ;
 
-    py::class_<LowFive::MetadataVOL>(m, "MetadataVOL", "metadata VOL object")
+    py::class_<LowFive::MetadataVOL> metadata_vol(m, "MetadataVOL", "metadata VOL object", vol_base);
+    metadata_vol
         .def(py::init<>(), "construct the object")
         .def("set_passthru",   &LowFive::MetadataVOL::set_passthru, "filename"_a, "pattern"_a, "set (filename,pattern) for passthru")
         .def("set_memory",     &LowFive::MetadataVOL::set_memory,   "filename"_a, "pattern"_a, "set (filename,pattern) for memory")
@@ -24,10 +27,24 @@ PYBIND11_MODULE(_lowfive, m)
 
     using communicator  = LowFive::DistMetadataVOL::communicator;
     using communicators = LowFive::DistMetadataVOL::communicators;
-    py::class_<LowFive::DistMetadataVOL>(m, "DistMetadataVOL", "metadata VOL object")
+    py::class_<LowFive::DistMetadataVOL> dist_metadata_vol(m, "DistMetadataVOL", "metadata VOL object", metadata_vol);
+    dist_metadata_vol
         .def(py::init<communicator, communicator>(),  "local"_a, "intercomm"_a,  "construct the object")
         .def(py::init<communicator, communicators>(), "local"_a, "intercomms"_a, "construct the object")
         .def("set_intercomm",   &LowFive::DistMetadataVOL::set_intercomm, "filename"_a, "pattern"_a, "index"_a, "set (filename,pattern) -> intercomm index")
         .def("serve_all",       &LowFive::DistMetadataVOL::serve_all, "serve all datasets")
     ;
+
+    py::class_<communicator>(m, "MPIComm")
+        .def(py::init<>())
+        .def(py::init([](long comm_)
+             {
+                return new communicator(*static_cast<MPI_Comm*>(reinterpret_cast<void*>(comm_)));
+             }))
+        .def_property_readonly("size", &communicator::size)
+        .def_property_readonly("rank", &communicator::rank)
+        .def_property_readonly("comm", &communicator::handle)
+    ;
+
+
 }
--- bindings/python/lowfive/__init__.py
+++ bindings/python/lowfive/__init__.py
@@ -6,11 +6,11 @@ init = DistMetadataVOL.__init__
 def convert_mpi_comm(self, local, intercomm):
     from mpi4py import MPI
 
-    local = MPI._addressof(local)
+    local = MPIComm(MPI._addressof(local))
     if type(intercomm) == list:
-        intercomm = [MPI._addressof(comm) for comm in intercomm]
+        intercomm = [MPIComm(MPI._addressof(comm)) for comm in intercomm]
     else:
-        intercomm = MPI._addressof(intercomm)
+        intercomm = MPIComm(MPI._addressof(intercomm))
 
     init(self, local, intercomm)
 DistMetadataVOL.__init__ = convert_mpi_comm
