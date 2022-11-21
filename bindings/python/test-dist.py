23f2fc7dc1907c25bcf5970958d25f0bf3761b67
Dmitriy Morozov <dmitriy@mrzv.org>
2022-11-21 11:10:57 -0800

    Update test-dist.py to be able to run with more than 2 ranks

--- bindings/python/test-dist.py
+++ bindings/python/test-dist.py
@@ -15,6 +15,8 @@ if not file and not memory:
 import lowfive
 from mpi4py import MPI
 
+lowfive.create_logger("debug")
+
 world = MPI.COMM_WORLD
 size  = world.Get_size()
 rank  = world.Get_rank()
@@ -39,8 +41,9 @@ if memory:
 import numpy as np
 import h5py
 if producer:
+    print(f"Producer: {local.rank} out of {local.size}")
     with h5py.File("out.h5", "w") as f:
-        f.create_dataset("data", data=np.ones((4, 3, 2), 'f'))
+        f.create_dataset(f"data{local.rank}", data=np.ones((4, 3, 2), 'f'))
         vol.print_files()
 
     if file:
@@ -49,6 +52,7 @@ else:
     if file:
         intercomm.Barrier()
 
+    print(f"Consumer: {local.rank} out of {local.size}")
     with h5py.File("out.h5", "r") as f:
-        data = f["data"]
+        data = f[f"data{local.rank}"]
         print("Consumer:", data)
