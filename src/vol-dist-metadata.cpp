f725d6ff7c9be0ba00c6cad3a058d6dc1c4d8409
Orcun Yildiz <oyildiz@bdw-0066.lcrc.anl.gov>
2023-02-16 12:16:20 -0600

    skip serving when there are no selected intercomms with serve_indices callback

--- src/vol-dist-metadata.cpp
+++ src/vol-dist-metadata.cpp
@@ -49,12 +49,16 @@ serve_all(bool delete_data)
     for (auto idx : indices)
         selected_intercomms.push_back(intercomms[idx]);
 
-    Index index(local, selected_intercomms, &files);
-    // we only serve if there any datasets; this matched old (pre-RPC)
-    // behavior, but is probably not the right universal solution; we should
-    // make this behavior user-configurable
-    if (index.indexed_datasets)
-        index.serve();
+    if (!selected_intercomms.empty())
+    {
+        Index index(local, selected_intercomms, &files);
+        // we only serve if there any datasets; this matched old (pre-RPC)
+        // behavior, but is probably not the right universal solution; we should
+        // make this behavior user-configurable
+        if (index.indexed_datasets)
+            index.serve();
+
+    }
 
     // TODO: what do we do about delete_data? Probably should become "clear files"
     //if (delete_data)
