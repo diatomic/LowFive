6b75da1f72635947c4664d78d14d1e6803a12618
Orcun YILDIZ <oyildiz@Orcuns-MacBook-Pro-2.local>
2023-03-07 16:56:57 -0600

    modifying spack recipe to setup L5/HDF5 env variables automatically

--- packages/lowfive/package.py
+++ packages/lowfive/package.py
@@ -17,11 +17,14 @@ class Lowfive(CMakePackage):
     version('tom-group-get', branch='tom-group-get')    # TODO: remove when no longer needed
 
     variant("examples", default=False, description="Install the examples")
+    variant("auto_load", default=True, description="Set LowFive environment variables")
 
     # TODO: uncomment following if installing prior to scorpio-example and want to match
 #     depends_on('mpich@4.0.2 device=ch3')
     depends_on('mpich')
     depends_on('hdf5+mpi+hl@1.12.1 ^mpich', type='link')
+
+    #extends("python") #TODO: Should be added for bringing pylowfive into PYTHONPATH
 
     def cmake_args(self):
         args = ['-DCMAKE_C_COMPILER=%s' % self.spec['mpi'].mpicc,
@@ -29,3 +32,8 @@ class Lowfive(CMakePackage):
                 '-DBUILD_SHARED_LIBS=false',
                 self.define_from_variant('lowfive_install_examples', 'examples')]
         return args
+
+    def setup_run_environment(self, env):
+        if "+auto_load" in self.spec:
+            env.set('HDF5_VOL_CONNECTOR', "lowfive under_vol=0;under_info={};")
+            env.set('HDF5_PLUGIN_PATH', self.prefix.lib)
