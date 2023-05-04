3a70afa887db79cb7b2765b3dafa3d395c2cc09b
Orcun YILDIZ <oyildiz@Orcuns-MacBook-Pro-2.local>
2023-05-04 12:16:49 -0500

    fixing unset_dist_callbacks at init.py

--- bindings/python/lowfive/__init__.py
+++ bindings/python/lowfive/__init__.py
@@ -3,7 +3,8 @@ from ._lowfive import _create_DistMetadataVOL, _create_MetadataVOL
 
 import atexit
 
-vols = []
+vols      = []
+dist_vols = []
 
 def create_MetadataVOL():
     vol = _create_MetadataVOL()
@@ -13,12 +14,15 @@ def create_MetadataVOL():
 
 def create_DistMetadataVOL(local, intercomm):
     vol = _create_DistMetadataVOL(local, intercomm)
-    vols.append(vol)
+    dist_vols.append(vol)
     return vol
 
 def unset_vol_callbacks():
-    for vol in vols:
+    for vol in dist_vols:
         vol.unset_dist_callbacks()
+        vol.unset_callbacks()
+
+    for vol in vols:
         vol.unset_callbacks()
 
 atexit.register(unset_vol_callbacks)
