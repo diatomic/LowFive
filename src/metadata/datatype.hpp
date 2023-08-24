59519e69b22b6215cd5945741d2ef46345ed6d3e
Dmitriy Morozov <dmitriy@mrzv.org>
2023-08-24 13:15:45 -0700

    Always make a copy of the datatype

--- src/metadata/datatype.hpp
+++ src/metadata/datatype.hpp
@@ -18,13 +18,9 @@ struct Datatype: Hid
     DatatypeClass           dtype_class;
     size_t                  dtype_size;         // in bytes
 
-    // DM: We never take ownership of a data type.
-    // Either way seems to work for built-in type, but once we make a copy of a
-    // type (and possibly commit, although that's less clear), incrementing its
-    // reference count, which is what owned does, breaks closing of the types.
-    // I'm not quite sure why it works this way.
-            Datatype(hid_t dtype_id_ = 0, bool owned = true):
-                Hid(dtype_id_, owned)
+    // always copy datatypes
+            Datatype(hid_t dtype_id_ = 0, bool owned = false):
+                Hid(dtype_id_ ? H5Tcopy(dtype_id_) : dtype_id_, true)
     {
         if (id == 0) return;
 
